fe49566d487087ee40f0caf3a16beaccfcf1cf15
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 09:46:57 +0200

    Finito e testato

--- App_MultiThreading/MainWindow.xaml.cs
+++ App_MultiThreading/MainWindow.xaml.cs
@@ -24,5 +24,58 @@ namespace App_MultiThreading
         {
             InitializeComponent();
         }
+
+        private void Btn_Task_Click(object sender, RoutedEventArgs e)
+        {
+            //DoWork();
+            //Creazione di un thread
+            Task.Factory.StartNew(DoWork);
+        }
+
+        private void DoWork()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                for (int j = 0; j < 10000; j++)
+                {
+
+                }
+            }
+
+            //AggiornaInterfaccia();
+            Dispatcher.Invoke(AggiornaInterfaccia);
+        }
+
+        private void AggiornaInterfaccia()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AggiornaInterfaccia(int j)
+        {
+            Lbl_Risultato.Content = "Finito";
+        }
+
+        private void Btn_Conta_Click(object sender, RoutedEventArgs e)
+        {
+            //DoCount();
+            Task.Factory.StartNew(AggiornaInterfaccia);
+        }
+
+        private void DoCount()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                for (int j = 0; j < 10000; j++)
+                {
+                    Dispatcher.Invoke(() => AggiornaInterfaccia(j));
+                }
+            }
+        }
+
+        private void AggiornaInterfacia(int j)
+        {
+            Lbl_Conteggio.Content = j.ToString();
+        }
     }
 }
